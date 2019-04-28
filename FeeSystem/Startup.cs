07b7cdc5428bc4a3519fdad9ac7b5789ff5e9b9d
Patryk Binicki <pbinicki@gmail.com>
2019-04-28 13:12:28 +0200

    Created MockResidentRepository

--- FeeSystem/Startup.cs
+++ FeeSystem/Startup.cs
@@ -1,10 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using FeeSystem.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace FeeSystem
@@ -14,6 +10,7 @@ namespace FeeSystem
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddTransient<IResidentRepository, MockResidentRepository>();
             services.AddMvc();
         }
 
