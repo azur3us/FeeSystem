54554cefa88158140bd1720b6ddaf6a473c3df29
Patryk Binicki <pbinicki@gmail.com>
2019-04-28 13:23:42 +0200

    Added HomeController

---
+++ FeeSystem/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using FeeSystem.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FeeSystem.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IResidentRepository _residentRepository;
+
+        public HomeController(IResidentRepository residentRepository)
+        {
+            _residentRepository = residentRepository;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
