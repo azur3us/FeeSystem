485d092d7b8603767a9ca58df7ba8c6d45cf2786
Patryk Binicki <pbinicki@gmail.com>
2019-05-03 16:04:29 +0200

    Visible payment details

--- FeeSystem/Logic/FeeCalculator.cs
+++ FeeSystem/Logic/FeeCalculator.cs
@@ -6,7 +6,7 @@ namespace FeeSystem.Logic
     public static class FeeCalculator
     {
 
-        
+        /*
         public static decimal PaymentDetails(this Resident resident)
         {
             decimal summaryOfCosts;
@@ -26,8 +26,31 @@ namespace FeeSystem.Logic
 
             return summaryOfCosts;
         }
-        
-       
-        
+        */
+        public static (decimal costOfExploitation, decimal costOfRepairFund, decimal costOfHotWaterConsumption,
+                      decimal costOfColdWaterConsumption, decimal costOfSewage, decimal costOfCentralHeating,
+                      decimal menagmentCost, decimal summaryOfCosts) PaymentDetails(this Resident resident)
+        {
+            decimal costOfExploitation = resident.MetersOfFlat * 0.6m;
+            decimal costOfRepairFund = resident.MetersOfFlat * 1.2m;
+            decimal costOfHotWaterConsumption = resident.HotWaterConsumption * 20;
+            decimal costOfColdWaterConsumption = resident.ColdWaterConsumption * 4;
+            decimal costOfSewage = (resident.HotWaterConsumption + resident.ColdWaterConsumption) * 5.51m;
+            decimal costOfCentralHeating = resident.MetersOfFlat * 4;
+            decimal menagmentCost = resident.MetersOfFlat * 0.4m;
+
+            
+            decimal summaryOfCosts = costOfExploitation + costOfRepairFund + costOfHotWaterConsumption + costOfColdWaterConsumption +
+                        costOfSewage + costOfCentralHeating + menagmentCost;
+
+
+            
+
+
+
+            return (costOfExploitation, costOfRepairFund, costOfHotWaterConsumption, costOfColdWaterConsumption, costOfSewage,
+                    costOfCentralHeating, menagmentCost, summaryOfCosts);
+        }
+
     }
 }
